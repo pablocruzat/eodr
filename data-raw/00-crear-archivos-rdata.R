6d996b7d7c98ed6b72316696c418ca7da3f45019
pablocruzat <53615718+pablocruzat@users.noreply.github.com>
2021-01-28 20:43:41 -0300

    test y vignette del paquete

--- data-raw/00-crear-archivos-rdata.R
+++ data-raw/00-crear-archivos-rdata.R
@@ -4,7 +4,6 @@ library(usethis)
 library(dplyr)
 finp <- list.files("data-raw", pattern = "\\.tsv", full.names = T, recursive = T)
 #parto con la primera tabla
-d <- read_delim(finp[1], delim = "\t")
 n <- gsub('.*/', "", finp)
 n <- gsub('_|[0-9]','', n)
 n <- tolower(gsub('\\.tsv','',n))
@@ -14,7 +13,7 @@ try(dir.create('inst/extdata'))
 con <- dbConnect(SQLite(), "inst/extdata/eod2017.sqlite")
 
 for (i in seq_along(finp)) {
-  d <- read_delim(finp[i], delim = '\\t')
+  d <- read_delim(finp[i], delim = '\t')
   copy_to(con, d, n[i], temporary = FALSE)
 }
 
