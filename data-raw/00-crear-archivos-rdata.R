87f11eabb919c6964664ba25d554a33e42157697
pablocruzat <53615718+pablocruzat@users.noreply.github.com>
2021-01-28 19:31:37 -0300

    bbdd inicial

---
+++ data-raw/00-crear-archivos-rdata.R
@@ -0,0 +1,22 @@
+library(readr)
+library(RSQLite)
+library(usethis)
+library(dplyr)
+finp <- list.files("data-raw", pattern = "\\.tsv", full.names = T, recursive = T)
+#parto con la primera tabla
+d <- read_delim(finp[1], delim = "\t")
+n <- gsub('.*/', "", finp)
+n <- gsub('_|[0-9]','', n)
+n <- tolower(gsub('\\.tsv','',n))
+
+try(dir.create('inst'))
+try(dir.create('inst/data'))
+con <- dbConnect(SQLite(), "data-raw/eod2017.sqlite")
+
+for (i in seq_along(finp)) {
+  d <- read_delim(finp[i], delim = '\\t')
+  copy_to(con, d, n[i], temporary = FALSE)
+}
+
+dbListTables(con)
+dbDisconnect(con)
